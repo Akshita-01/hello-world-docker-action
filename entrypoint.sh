52065cd0de8d629da0dc02b6a9b77cd57fe82791
Akshita-01 <76248217+Akshita-01@users.noreply.github.com>
2021-04-07 17:07:00 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,3 +5,8 @@ time=$(date)
 echo "::set-output name=time::$time"
 
 $ chmod +x entrypoint.sh
+
+git add action.yml entrypoint.sh Dockerfile README.md
+git commit -m "My first action is ready"
+git tag -a -m "My first action release" v1
+git push --follow-tags
